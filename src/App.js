ce20fb1d4c6bfdf109e45888c4209adf395b983a
chidinma-elekwachi <elexisbiz@gmail.com>
2025-04-21 17:55:58 +0100

    swe-port

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Crating my portfolio from scratch</p>
+    </div>
+  );
+}
+
+export default App;
