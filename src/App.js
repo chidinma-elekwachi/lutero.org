89f5006b82846ef9520ad3480107c98a38dbbf65
chidinma-elekwachi <elexisbiz@gmail.com>
2025-07-31 17:58:19 +0100

    navbar routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Premium from "./pages/Premium";
+import Terms from "./pages/Terms";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <p>Starting</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/premium" element={<Premium />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
