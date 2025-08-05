d3ea650df766bcb4e786c1d40bd30547cc21e5a9
chidinma-elekwachi <elexisbiz@gmail.com>
2025-08-05 14:36:23 +0100

    hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,60 @@
-import React from 'react'
+import {useState} from 'react'
 import { NavLink } from 'react-router-dom';
 import googlePlay from '../assets/google-play-store logo.png';
 import appStore from '../assets/app-store-logo.png';
 import '../pages/styles.css';
 
 function Navbar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+  
+    const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
      <nav className="navbar">
         <a href="/"><img src='/lutero-icon.png' alt="App Store" className='navbar-link'/></a>
-        <div className="nav-links">
+        <div className="hamburger" onClick={handleToggle}>
+          <span className={`bar ${isOpen ? 'open' : ''}`}></span>
+          <span className={`bar ${isOpen ? 'open' : ''}`}></span>
+          <span className={`bar ${isOpen ? 'open' : ''}`}></span>
+        </div>
+        <div className={`nav-links ${isOpen ? 'open' : ''}`}>
         <NavLink 
           to="/"
+          onClick={closeMenu}
           className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
         >
           Home
         </NavLink>
         <NavLink 
           to="/premium"
+          onClick={closeMenu}
           className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
         >
           Premium
         </NavLink>
         <NavLink 
           to="/terms"
+          onClick={closeMenu}
           className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
         >
           Terms
         </NavLink>
         <NavLink 
           to="/contact"
+          onClick={closeMenu}
           className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
         >
           Contact Us
         </NavLink>
-          <a href="/"><img src={appStore} alt="App Store" /></a>
-          <a href="/"><img src={googlePlay} alt="Google Play" /></a>
+          <a href="/" onClick={closeMenu}><img src={appStore} alt="App Store" /></a>
+          <a href="/" onClick={closeMenu}><img src={googlePlay} alt="Google Play" /></a>
         </div>
       </nav>
   )
